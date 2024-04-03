4e07aaab70956109f48a54e8e942caff3803adfb
Cristian <aarevalo.cristian.17@gmail.com>
2024-04-03 14:56:07 -0500

    being

---
+++ src/DatosPrimitivos.java
@@ -0,0 +1,16 @@
+public class DatosPrimitivos {
+    public static void main(String args[]){
+        int miVariableEntera = 3;
+        System.out.println(miVariableEntera);
+
+        //Variable mofidico
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+
+        miVariableCadena = "Adios";
+        System.out.println(miVariableCadena);
+    }
+}
